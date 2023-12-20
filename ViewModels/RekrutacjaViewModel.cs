060a52edef3a993e3254afc6df85ea47711d0b1c
Adam <al.lo3opole@gmail.com>
2023-12-20 22:32:52 +0100

    Remove enum classes and replace them with enums

--- ViewModels/RekrutacjaViewModel.cs
+++ ViewModels/RekrutacjaViewModel.cs
@@ -18,17 +18,41 @@ namespace projektowaniaOprogramowania.ViewModels
         public DateTime DataZamknieciaRekrutacji { get; set; }
 
         public DateTime DataZamknieciaPrzyjec { get; set; }
+        
+        public StopienStudiow StopienStudiow { get; set; }
+        
+        public StatusRekrutacji StatusRekrutacji { get; set; }
 
-        [ForeignKey("StatusRekrutacji")]
-        public string StatusRekrutacjiEnumLiteral { get; set; }
-        public StatusRekrutacjiViewModel StatusRekrutacji { get; set; }
+        // [ForeignKey("StatusRekrutacji")]
+        // public string StatusRekrutacjiEnumLiteral { get; set; }
+        // public StatusRekrutacjiViewModel StatusRekrutacji { get; set; }
 
-        [ForeignKey("StopienStudiow")]
-        public string StopienStudiowEnumLiteral { get; set; }
-        public StopienStudiowViewModel StopienStudiow { get; set; }
+        // [ForeignKey("StopienStudiow")]
+        // public string StopienStudiowEnumLiteral { get; set; }
+        // public StopienStudiowViewModel StopienStudiow { get; set; }
 
-        [ForeignKey("SemestrRekrutacji")]
-        public string SemestrRekrutacjiEnumLiteral { get; set; }
-        public SemestrRekrutacjiViewModel SemestrRekrutacji { get; set; }
+        // [ForeignKey("SemestrRekrutacji")]
+        // public string SemestrRekrutacjiEnumLiteral { get; set; }
+        public SemestrRekrutacji SemestrRekrutacji { get; set; }
+    }
+
+    public enum StopienStudiow
+    {
+        Pierwszy,
+        Drugi
+    }
+
+    public enum StatusRekrutacji
+    {
+        Otwarta,
+        Zawieszona,
+        Zamknieta,
+        Rozpatrywana
+    }
+
+    public enum SemestrRekrutacji
+    {
+        Letni,
+        Zimowy
     }
 }
